b331cf43777512404912c9da28cb2347077c2259
Felipe Marchi <felipe.marchi@datasystem.com.br>
2023-10-19 08:55:09 -0300

    Primeiro commit - Subindo arquivos do projeto

---
+++ Marvel/Shared/Funcoes.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Marvel.Shared
+{
+    public class Funcoes
+    {
+        public static string GerarHash(
+            string ts, string publicKey, string privateKey)
+        {
+            byte[] bytes =
+                Encoding.UTF8.GetBytes(ts + privateKey + publicKey);
+            var gerador = MD5.Create();
+            byte[] bytesHash = gerador.ComputeHash(bytes);
+            return BitConverter.ToString(bytesHash)
+                .ToLower().Replace("-", String.Empty);
+        }
+    }
+}
